5e70d140723396854ab33dd0e2ad8fae13eb179f
Arif Hossain <arif.hossain@iths.se>
2021-04-11 21:40:03 +0200

    EOF(eND Of File) class

---
+++ src/main/java/dataTypes/endOfFile.java
@@ -0,0 +1,12 @@
+package dataTypes;
+
+public class endOfFile {
+    public static void main ( String[] args ) {
+
+    }
+}
+/*
+"In computing, End Of File (commonly abbreviated EOF) is a condition
+in a computer operating system where no more data can be read from a
+data source." — (Wikipedia: End-of-file)
+ */
